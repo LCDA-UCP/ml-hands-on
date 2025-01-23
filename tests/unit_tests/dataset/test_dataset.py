7c83a947dd03fe693ef968b838c2c9b948b9abd8
João Correia <jfscorreia95@gmail.com>
2025-01-23 17:56:38 +0000

    [ADD] dataset test

---
+++ tests/unit_tests/dataset/test_dataset.py
@@ -0,0 +1,16 @@
+import unittest
+
+import numpy as np
+
+from ml_hands_on.data import Dataset
+
+
+class TestDataset(unittest.TestCase):
+
+    def test_dataset_shape(self):
+
+        X = np.array([[1, 2, 3], [4, 5, 6]])
+        y = np.array([1, 2])
+
+        dataset = Dataset(X, y, features=['a', 'b', 'c'], label='y')
+        self.assertEqual((2, 3), dataset.shape())
