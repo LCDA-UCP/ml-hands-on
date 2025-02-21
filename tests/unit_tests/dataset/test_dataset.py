e71305f254e22ce96cc2759e88eeb8e6ad690501
Daniel Rodrigues <danielalejandrocuberos@gmail.com>
2025-02-21 12:13:00 +0000

    add test_dataset_label

--- tests/unit_tests/dataset/test_dataset.py
+++ tests/unit_tests/dataset/test_dataset.py
@@ -14,3 +14,12 @@ class TestDataset(unittest.TestCase):
 
         dataset = Dataset(X, y, features=['a', 'b', 'c'], label='y')
         self.assertEqual((2, 3), dataset.shape())
+
+    def test_dataset_label(self):
+        X = np.array([[1, 2, 3], [4, 5, 6]])
+        y = np.array([1, 2])
+        dataset = Dataset(X)
+        self.assertFalse(dataset.has_label())
+
+        dataset = Dataset(X,y)
+        self.assertTrue(dataset.has_label())
