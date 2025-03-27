6895dfb5ccd614c2c5a7aba6db29a4664b319353
Cleópatra Santos <cleopatraadryansantos@gmail.com>
2025-03-27 19:24:22 +0000

    Created the test_varience_threshold class and implemented the test methods: : def test_fit, test_transform, test_transform_no_removal , test_transform_all_removed

--- tests/unit_tests/dataset/test_variance_threshold.py
+++ tests/unit_tests/dataset/test_variance_threshold.py
@@ -2,14 +2,56 @@ import unittest
 import numpy as np
 import pandas as pd
 
+from ml_hands_on import VarianceThreshold
 from ml_hands_on.data import Dataset
-from ml_hands_on.feature_selection import variance_threshold
 
 
-class  testVariance(unittest.TestCase):
+class  test_varience_threshold(unittest.TestCase):
 
-       def test_transform(self):
+        def test_fit(self):
 
-           X = np.array([['1','4','20','10'],['7','5','20','10'],['0','9','40','10']])
-           dataset = Dataset(X,features=)
+           X = np.array([[1,2,3],[1,2,3],[1,2,3]])
 
+           dataset = Dataset(X=X, y =None,features=['f1','f2','f3'])
+
+           vt = VarianceThreshold()
+           vt._fit(dataset)
+
+           expected_variance = np.var(X, axis=0)
+           assert np.array_equal(vt.variance, expected_variance)
+
+
+
+        def test_transform(self):
+            X = np.array([[1, 2, 3], [1, 2, 4], [1, 2, 5]])
+            dataset = Dataset(X=X, y=None, features=["f1", "f2", "f3"])
+
+            vt = VarianceThreshold(threshold=0.2)
+            vt._fit(dataset)
+            new_dataset = vt._transform(dataset)
+
+            assert new_dataset.X.shape[1] == 1
+            assert new_dataset.features == ["f3"]
+
+
+        def test_transform_no_removal(self):
+            X = np.array([[6, 2, 4], [1, 26, 15], [3, 9, 5]])
+            dataset = Dataset(X=X, y=None, features=["f1", "f2", "f3"])
+
+            vt = VarianceThreshold(threshold=0.5)
+            vt._fit(dataset)
+            new_dataset = vt._transform(dataset)
+
+            assert new_dataset.X.shape[1]== 3
+            assert new_dataset.features == ["f1", "f2", "f3"]
+
+        def test_transform_all_removed(self):
+            X = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+            dataset = Dataset(X=X, y=None, features=["f1", "f2", "f3"])
+
+            vt = VarianceThreshold(threshold=0.2)
+            vt._fit(dataset)
+            new_dataset = vt._transform(dataset)
+
+            assert new_dataset.X.shape[1]== 0
+            assert new_dataset.features == []
