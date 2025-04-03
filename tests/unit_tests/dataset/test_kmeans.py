40c6e009099cd0b44df77109b12e89ea683636ad
D1ana-bd <diana.doria2005@gmail.com>
2025-04-03 11:46:16 +0100

    Assignment 3

--- tests/unit_tests/dataset/test_kmeans.py
+++ tests/unit_tests/dataset/test_kmeans.py
@@ -9,7 +9,7 @@ class test_kmeans(unittest.TestCase):
     def test_fit(self):
         X = np.array([[1, 2], [1, 4], [1, 0],
                       [10, 2], [10, 4], [10, 0]])
-        dataset = Dataset(X=X, y=None, features=["f1", "f2"])
+        dataset = Dataset(X=X, y=None)
 
         model = KMeans(k=2, max_iter=100, distance_function=euclidean_distance)
         model._fit(dataset)
@@ -21,25 +21,26 @@ class test_kmeans(unittest.TestCase):
     def test_transform(self):
         X = np.array([[1, 2], [1, 4], [1, 0],
                       [10, 2], [10, 4], [10, 0]])
-        dataset = Dataset(X=X, y=None, features=["f1", "f2"])
+        dataset = Dataset(X=X, y=None)
 
         model = KMeans(k=2, max_iter=100, distance_function=euclidean_distance)
         model._fit(dataset)
 
         transformed = model._transform(dataset)
-        assert transformed.X.shape == (6, 2)  # 6 samples, 2 centroids
+        assert transformed.X.shape == (6, 2)
 
     def test_predict(self):
         X_train = np.array([[1, 2], [1, 4], [1, 0],
                             [10, 2], [10, 4], [10, 0]])
-        train_set = Dataset(X=X_train, y=None, features=["f1", "f2"])
+        train_set = Dataset(X=X_train, y=None)
 
         model = KMeans(k=2, max_iter=100, distance_function=euclidean_distance)
         model._fit(train_set)
 
         X_test = np.array([[0, 0], [12, 3]])
-        test_set = Dataset(X=X_test, y=None, features=["f1", "f2"])
+        test_set = Dataset(X=X_test, y=None)
 
         preds = model._predict(test_set)
         assert preds.shape == (2,)
         assert all(p in [0, 1] for p in preds)
+
